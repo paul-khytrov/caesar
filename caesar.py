017cc12f2fa8055b9007e9fe70254991a6288444
paul-khytrov <72658457+paul-khytrov@users.noreply.github.com>
2020-11-08 14:57:59 +0300

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,18 @@
+a = input()
+a = a.upper()
+b = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+d = ""
+for i in range(len(a)):
+        if a[i] == "X":
+            d = d + a[i].replace(a[i], "A")
+        elif a[i] == "Y":
+            d = d + a[i].replace(a[i], "B")
+        elif a[i] == "Z":
+            d = d + a[i].replace(a[i], "C")
+        elif a[i] == " ":
+            d = d + a[i].replace(a[i], " ")
+        elif not a[i].isalpha():
+            d = d + a[i]
+        else:
+            d = d + a[i].replace(a[i], b[b.index(a[i])+3])
+print(d.capitalize())
